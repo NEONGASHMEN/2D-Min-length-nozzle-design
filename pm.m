c333a20dcad14f77c95eaec2198c6b81aae282cc
NEONGASHMEN <83530865+NEONGASHMEN@users.noreply.github.com>
2022-07-23 13:09:07 +0530

    Fix: Error in Prandtl-Meyer fn

--- pm.m
+++ pm.m
@@ -1,5 +1,5 @@
 function nu = pm(M,gamma)
 temp_gamma = sqrt((gamma+1)/(gamma-1));
 temp_M = sqrt(M*M - 1);
-nu = (temp_gamma*atan(temp_gamma*temp_M)) - atan(temp_M);
+nu = (temp_gamma*atan(inv(temp_gamma)*temp_M)) - atan(temp_M);
 end
